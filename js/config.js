9a01d9ef55d2003fa8ced2cd2d16c65fcf784155
vulcanbaby <127916440+vulcanbaby@users.noreply.github.com>
2025-11-25 05:35:45 +0800

    Comment out PASSWORD_CONFIG in config.js
    Commented out the PASSWORD_CONFIG object in config.js.

--- js/config.js
+++ js/config.js
@@ -5,10 +5,10 @@ const SEARCH_HISTORY_KEY = 'videoSearchHistory';
 const MAX_HISTORY_ITEMS = 5;
 
 // 密码保护配置
-const PASSWORD_CONFIG = {
+//const PASSWORD_CONFIG = {
     //localStorageKey: 'passwordVerified',  // 存储验证状态的键名
     //verificationTTL: 90 * 24 * 60 * 60 * 1000,  // 验证有效期（90天，约3个月）
-};
+//};
 
 // 网站信息配置
 const SITE_CONFIG = {
